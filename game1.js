30c9b91ec707f4a150909209a00654b32f0d773b
sonndev <54797284+sonndev@users.noreply.github.com>
2019-09-27 13:46:41 +0200

    Add files via upload

--- game1.js
+++ game1.js
@@ -11,6 +11,7 @@ var attempts = 0;
 var maxGuesses = 10;
 
 
+
 function gameEnded(){
     $('newGameBtn').style.display = 'inline';
     $("easyBtn").style.display = 'none';
@@ -78,6 +79,7 @@ function compareGuess() {
 
 
 
+
 // Create, read and erease cookies 
 function createCookie(name, value, days) {
     let expires;
@@ -104,18 +106,37 @@ function readCookie(name) {
             return c.substring(nameEQ.length, c.length);
         }
     }
-    return "";
+    return null;
 }
 
 function eraseCookie(name) {
     createCookie(name, "", -1);
 }
 
-const showCookies = function () { // function that doesnt work 
-    $('showCookie').style.display = "inline";
-    $('showCookie').innerHTML = document.cookie;
+
+
+// function to show highscore 
+function showScore() {
+    let cookietxt = document.cookie; // reads all cookies 
+    let arr = cookietxt.split(';'); // splits all cookies elements after ; 
+    for (let i = 0; i < arr.length; i++)  { // create a loop for the whole array 
+        let tr = document.createElement('tr'); // create table row in html
+        let arr1 = arr[i].split('='); // splits name and value 
+        let td = document.createElement('td'); // creates the table data
+        let arrayTxt = document.createTextNode(arr1[0]); // array content 
+
+        // Why we don't use let here or (declare it to a new variable) is because we ABOVE declared e.g ((let td = 5;)) so if we want to change the value of the variable td later we can just
+        // write ((td = 10;))
+        td.appendChild(arrayTxt); // input table content
+        tr.appendChild(td); // input table data in to a row
+        td = document.createElement('td'); // change varible
+        arrayTxt = document.createTextNode(arr1[1]); // create value in table 
+        td.appendChild(arrayTxt);
+        tr.appendChild(td);
+        $('showScore').appendChild(tr); // input to table in html file
+    }
 }
-showCookies();
+
 
 //This works, and saves cookies, also displays it... FIGURE OUT HOW TO DISPLAY AT ALL TIMES.
 let saveScore = function () {
@@ -123,25 +144,23 @@ let saveScore = function () {
 
     if (pName.length > 0) {
         $('playerScore').innerHTML = $('playerName').value;
-        $('playerAttempts').innerHTML = "guessed it in" + attempts;
-        $('showCookie').innerHTML = document.cookie;
+        $('playerAttempts').innerHTML = "guessed it in" + " " + attempts;
+        
         createCookie(pName, attempts, 14);
-    } else {
-        console.log(document.cookie);
     }
 }
-
-
 
 
 //#endregion
 
 let windowLoad = function () {
+    
     $('newGameBtn').addEventListener('click', newGame);
     $("inputGuess").addEventListener('change', compareGuess);
     $('saveScoreToBoard').addEventListener('click', saveScore);
+    showScore();
+    // $('showCookie').textContent = document.cookie;
     
-
 }
 
 window.addEventListener('load', windowLoad);
